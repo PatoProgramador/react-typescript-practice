25ead59fdc0b7b3dafced6ec6454f29a4af66319
PatoProgramador <spartan1171947@hotmail.com>
2023-05-30 10:07:53 -0500

    basic subs with typescritp inference of undefined properties

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [subs, setSubs] = useState([
+    {
+      nick: 'pato',
+      subMonths: 3,
+      avatar: 'https://i.pravatar.cc/150?u=pato',
+      description: 'An random pato of a random place'
+    },
+    {
+      nick: 'sergio',
+      subMonths: 5,
+      avatar: 'https://i.pravatar.cc/150?u=sergio',
+    }
+  ]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>subs</h1>
+      <ul>
+        {
+          subs.map((sub, index)=> {
+            return (
+              <li key={index}>
+                <img src={sub.avatar} alt={`Avatar for ${sub.nick}`} />
+                <h4>{sub.nick} (<small>{sub.subMonths}</small>)</h4>
+                <p>{sub.description?.substring(0,100)}</p>
+              </li>
+            )
+          })
+        }
+      </ul>
     </div>
   );
 }
