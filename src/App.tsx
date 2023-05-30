c10f1eb7762c754ba683e6698cf2b969af21bd14
PatoProgramador <spartan1171947@hotmail.com>
2023-05-30 10:12:02 -0500

    ts interfaces

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,34 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
+
+const inital_state = [
+  {
+    nick: 'pato',
+    subMonths: 3,
+    avatar: 'https://i.pravatar.cc/150?u=pato',
+    description: 'An random pato of a random place'
+  },
+  {
+    nick: 'sergio',
+    subMonths: 5,
+    avatar: 'https://i.pravatar.cc/150?u=sergio',
+  }
+] 
+
+interface Sub {
+  nick: string,
+  avatar: string,
+  subMonths: number,
+  description?: string
+}
 
 function App() {
 
-  const [subs, setSubs] = useState([
-    {
-      nick: 'pato',
-      subMonths: 3,
-      avatar: 'https://i.pravatar.cc/150?u=pato',
-      description: 'An random pato of a random place'
-    },
-    {
-      nick: 'sergio',
-      subMonths: 5,
-      avatar: 'https://i.pravatar.cc/150?u=sergio',
-    }
-  ]);
+  const [subs, setSubs] = useState<Array<Sub>>([]);
+
+  useEffect(() => {
+    setSubs(inital_state)
+  }, [])
 
   return (
     <div className="App">
